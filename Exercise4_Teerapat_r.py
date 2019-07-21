ad02492e8c21498c2129ca115e43c33f003590b6
icelake10 <52350278+icelake10@users.noreply.github.com>
2019-07-21 11:50:01 +0700

    Add files via upload

---
+++ Exercise4_Teerapat_r.py
@@ -0,0 +1,9 @@
+x="Foundation English"
+y="General Business"
+z="introductin to Computer Systems"
+c="computer Progarmming"
+print("---------Your Score---------")
+print(x,65,"คะแนน")
+print(y,56,"คะแนน")
+print(z,70,"คะแนน")
+print(c,59,"คะแนน")
